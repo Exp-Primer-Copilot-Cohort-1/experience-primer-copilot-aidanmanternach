37b13a9707e9997c9d888ab79027f261518a76c6
Aidan Manternach <92654166+aidanmanternach@users.noreply.github.com>
2023-11-07 05:29:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberIcon = document.getElementById("memberIcon");
+    var skillsIcon = document.getElementById("skillsIcon");
+    var projectsIcon = document.getElementById("projectsIcon");
+    var contactIcon = document.getElementById("contactIcon");
+    var aboutIcon = document.getElementById("aboutIcon");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberIcon.style.color = "#f5f5f5";
+    skillsIcon.style.color = "#000000";
+    projectsIcon.style.color = "#000000";
+    contactIcon.style.color = "#000000";
+    aboutIcon.style.color = "#000000";
+}
